f738fa867841c9ae9fe12c8df42e477cca29ed88
devin shields <devin.shields@gmail.com>
2014-05-10 04:26:52 -0400

    finished 77

--- python/problem077.py
+++ python/problem077.py
@@ -1,13 +1,50 @@
-#!/usr/bin/python
-#
-#
-#
+#!/usr/bin/env python
+'''   
+  http://projecteuler.net/problem=77
+'''
 
-
+import itertools
 import euler
 
-test_value = 10
-primes = euler.prime_sieve(test_value)[::-1]
 
-print primes
+def count(S, m, n):
+  ''' S - ascending list of globally available integer components (coins)
+      m - index into S[]. S[:m] is the set of allowed components for this run
+      n - target value for this run
+
+      math trick:
+          count(S, m, n) == count(S, m-1, n) + count(S, m, n - S[m-1])
+  '''
+  # base case, found exact change
+  if n == 0:
+    return 1
+  
+  # base case, overshot the target value / combo isn't feasable
+  if n < 0:
+    return 0
+
+  # base case, no smaller coins available
+  if m <= 0:
+      return 0
+  
+  # recurse!
+  return count(S, m-1, n) + count(S, m, n - S[m-1])
+
+
+def count_prime_summations(target_value):
+  ''' sieve for 'coin' values below the target
+      and pass to the combo counter '''
+  S = euler.prime_sieve(target_value+1)
+  return count(S, len(S), target_value)
+
+
+# test
+assert count_prime_summations(10) == 5
+
+
+for target_value in itertools.count(10):
+  if count_prime_summations(target_value) > 5000:
+    print 'answer: {0}'.format(target_value)
+    break
+
 
