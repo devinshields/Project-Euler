97d6b196d67337976c36889f7dbd4aeefb00019e
devin shields <devin.shields@gmail.com>
2014-05-13 01:27:55 -0400

    finished 98

--- python/problem098.py
+++ python/problem098.py
@@ -1,50 +1,80 @@
-#!/usr/bin/python -tt
-'''
-plan:
-  x) find all anagram pairs
-  x) try all posible substitutions that result in double square (must be efficient)
+#!/usr/bin/env python
+'''  '''
 
-'''
 
-import csv
-import collections
 import itertools
 
-AnagramPair = collections.namedtuple('AnagramPair', ['sorted_chars', 'word'])
 
-# get a word hashset and sorted word list
-word_set = set([l for l in csv.reader(open(r'./words.txt', 'rb'))][0])
-words    = sorted(word_set)
+# read in word data file
+words = sorted(set([l.strip()[1:-1] for l in open('words.txt', 'rb').read().split(',')]))
 
-#
-pairs = sorted(AnagramPair(sorted(w), w) for w in words)
 
-# grab just the sorted set of distinct words for all char equivalent word groups
-word_grps   = [sorted(set([p.word for p in grp])) for key, grp in itertools.groupby(pairs, lambda p: p.sorted_chars)]
+# group words into anagram sets
+anagram_word_groups = []
+for key, grp in itertools.groupby(sorted([(''.join(sorted(w)), w) for w in words]), lambda t: t[0]):
+  grp = list(grp)
+  if len(grp) > 1:
+    anagram_word_groups.append([t[1] for t in grp])
 
-# remove non-anagram words
-char_groups = filter(lambda grp: len(grp) > 1, word_grps)
 
-# ensure all anagram groups are len == 2
-anagram_pairs = sorted(itertools.chain(*map(lambda grp: list(itertools.combinations(grp, 2)), char_groups)))
+# get anagram word pairs rather than anagram groups
+anagram_pairs = []
+for grp in anagram_word_groups:
+  for pair in itertools.combinations(grp, 2):
+    anagram_pairs.append(pair)
 
-# test print
+
+# square anagram tester
+def all_possible_mappings(word0, word1, digits=tuple(range(9+1))):
+  keys = sorted(set(word0))
+  possible_char_permutation = itertools.permutations(digits, len(keys))
+  for values in possible_char_permutation:
+    mapping = {k:v for k, v in zip(keys, values)}
+    # forbid leading 0s
+    if mapping[word0[0]] and mapping[word1[0]]:
+      yield mapping
+
+
+def word_as_int(word, mapping):
+  return int(''.join([str(mapping[c]) for c in word]))
+
+
+def get_all_9_char_squares():
+  ''' non-floating point method for getting square integers '''
+  squares = []
+  for i in itertools.count(1):
+    sq = i**2
+    if sq > 10**10:
+      break
+    squares.append(sq)
+  return set(squares)
+
+
+square_set = get_all_9_char_squares()
+
+
+
+def words_are_square_anagram(word0, word1):
+  '''
+    try a bijective mapping from characters to digits
+    test if word0 is a square
+    test if word1 is a square
+  '''
+  assert tuple(sorted(word0)) == tuple(sorted(word1))
+
+  for mapping in all_possible_mappings(word0, word1):
+    if word_as_int(word0, mapping) in square_set:
+      if word_as_int(word1, mapping) in square_set:
+        yield max(word_as_int(word0, mapping), word_as_int(word1, mapping))
+
+# main
+largest_square_anagram = 0
 for p in anagram_pairs:
   print p
+  for square_anagram_int in words_are_square_anagram(p[0], p[1]):
+    largest_square_anagram = max(largest_square_anagram, square_anagram_int)
+    
+print
+print 'answer: {0}'.format(largest_square_anagram)
+print
 
-# matching the sorted, uniq chars in a word pair against this list 
-# will give all possible (char -> digit mappings)
-all_digit_perms = list(itertools.permutations(range(10)))
-
-# tool to generate all possible (char -> 0_9_digit) mappings
-#   remember that there are no leading 0s.
-def all_char_dig_mappings(pair):
-  for seq in all_digit_perms:
-    possible_char_map = {c:i for c,i in zip(sorted(set(pair[0])), seq)}
-    if possible_char_map[pair[0]] != 0 and possible_char_map[pair[1]] != 0: # check for no leading zeros
-      print possible_char_map
-      yield possible_char_map
-
-
-for d in all_char_dig_mappings(anagram_pairs[3]):
-  print d
