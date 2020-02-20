03a5e6c43beb4a691389ec68c57bf6de06963fe9
devin shields <devin.shields@gmail.com>
2020-02-20 00:45:02 -0500

    added solution to code, not just readme

--- problem_100/solve_100.py
+++ problem_100/solve_100.py
@@ -33,30 +33,42 @@ def test_prob_of_bb():
     assert .5 == prob_of_bb(120, 85)
 
 
-def solve_100(max_n):
+def solve_100_brute_force(max_n):
     """
     """
     for n in range(3, max_n):
         b = blue_disks(n)
         p = prob_of_bb(n, b)
         if p == .5:
-            #print('n: {}, b: {}, p: {}'.format(n, b, p))
-            #print(n)
-            print(b)
+            print('n: {}, b: {}, p: {}'.format(n, b, p))
             if n >= 10**12:
                 print('Project Euler Problem #100 Solution: {} blue disks'.format(b))
 
 
-def brute_force():
+def time_brute_force():
     """
     """
     t0 = time.time()
     max_n_exp = 7
     max_n = 10**max_n_exp
-    solve_100(max_n)
+    solve_100_brute_force(max_n)
     t1 = time.time()
     t_delta = t1 - t0
     print('brute force solution up to max_n: 10**{}, took: {} seconds'.format(max_n_exp, int(t_delta)))
+
+
+def solve_100():
+    """
+    using the brute force generated sequence of valid `n` values:
+    https://oeis.org/search?q=4%2C+21%2C+120%2C+697%2C+4060%2C+23661%2C+137904%2C+803761%2C+4684660%2C+27304197&language=english&go=Search
+    """
+    n_candidates = [4, 21, 120, 697, 4060, 23661, 137904, 803761, 4684660, 27304197, 159140520, 927538921, 5406093004, 31509019101, 183648021600, 1070379110497, 6238626641380, 36361380737781, 211929657785304, 1235216565974041]
+    for n in n_candidates:
+        b = blue_disks(n)
+        if (prob_of_bb(n, b) == .5) and (n >= 10**12):
+            print('Project Euler Problem #100 Solution: {} blue disks'.format(b))
+            break
+
 
 
 def main():
@@ -64,7 +76,8 @@ def main():
     """
     test_blue_disks()
     test_prob_of_bb()
-    brute_force()
+    time_brute_force()
+    solve_100()
 
 
 
